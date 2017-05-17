3cab9d4a4d5f8044520c54f15d800baa177aaa8c
xiecg <xiecg53@163.com>
2017-05-17 22:25:12 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HttpModule }    from '@angular/http';
+
+console.log('HttpModule', HttpModule);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app works!';
 }
