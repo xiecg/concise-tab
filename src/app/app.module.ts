4b1b065dd2f60fdbd23307651c84c39387ad2538
xiecg <xiecg53@163.com>
2017-05-23 23:12:28 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ContentComponent, MenuComponent } from '../components';
@@ -11,6 +11,7 @@ import { RenderBackgroundImageDirective } from '../directive/renderBackgroundIma
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule
   ],
--- src/components/menu/menu.component.ts
+++ src/components/menu/menu.component.ts
@@ -1,11 +1,63 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+      trigger('menuState', [
+        state('inactive', style({
+          transform: 'translateX(0px)'
+        })),
+        state('active', style({
+          transform: 'translateX(-300px)'
+        })),
+        transition('inactive => active', animate('200ms ease-in')),
+        transition('active => inactive', animate('200ms ease-out'))
+      ])
+    ]
 })
 
 export class MenuComponent {
+  private state: string;
+  private typeActive: string;
+  private menus: {
+    [index: number]: object;
+  };
   constructor() {}
+  ngOnInit () {
+    this.state = 'inactive';
+    this.menus = [{
+      icon: 'icon-bookmark',
+      name: '书签',
+      type: 'bookmark'
+    },{
+      icon: 'icon-apps',
+      name: '应用程序',
+      type: 'apps'
+    },{
+      icon: 'icon-extend',
+      name: '扩展程序',
+      type: 'extend'
+    },{
+      icon: 'icon-history',
+      name: '历史记录',
+      type: 'history'
+    },{
+      icon: 'icon-download',
+      name: '下载',
+      type: 'download'
+    }];
+  }
+  onOpenMenu (type) {
+    this.typeActive = type;
+    this.state = this.state === 'inactive' ? 'active' : 'inactive';
+  }
 }
